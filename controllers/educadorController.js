8e4d2ffee2af13415f0279ff0e2a8fe8705c4d4d
Gabriel Rosa <grosa.ict@gmail.com>
2021-11-08 22:19:25 -0300

    refatoring code

--- controllers/educadorController.js
+++ controllers/educadorController.js
@@ -3,52 +3,15 @@ const Educador          = require('../models/Educador');
 const usuarioService    = require('../services/usuario');
 
 exports.buscaEducador = async (req, res) => {
-    //req.user = { _id: '1234' } // APAGAR
-    req.user = { _id: req.params.id } // APAGAR
-
+    usuarioId = req.params.id
     try {
-        const reqEducador = {
-            usuarioToFind: {
-                id:     req.params.id,
-                papel:  1
-            },
-            userLoggedId: req.user._id
+        const educador = await Educador.findOne({ usuario: usuarioId }).exec();
+        if(!educador){
+            // 204 No Content
+            return res.status(204).send({ status: 204, message: 'Educador não encontrado' });
         }
-
-        const result = await usuarioService.buscaUsuarioPorPapel(reqEducador)
-        switch (result.status) {
-            case 403: // 403 Forbidden
-                res.status(result.status).send({ status: result.status, message: result.message });
-                break;
-            case 204: // 204 No Content
-                res.status(result.status).send({ status: result.status, message: 'Educador não encontrado' });
-                break;
-            case 200: // 200 OK
-                const educadorIncompleto = await Educador.findOne({ usuario: result.data.usuario._id }).exec();
-                if(!educadorIncompleto){
-                    // 204 No Content
-                    return res.status(204).send({ status: 204, message: 'Educador não encontrado' });
-                }
-                if(educadorIncompleto.usuario._id != req.user._id){
-                    // 403 Forbidden
-                    return res.status(403).send({ status: 403, message: 'Acesso negado' });
-                }
-                // 200 OK
-                const educador      =   {
-                    nome:       result.data.usuario.nome,
-                    email:      result.data.usuario.email,
-                    papel:      result.data.usuario.papel,
-                    _id:        result.data.usuario._id,
-                    idEducador: educadorIncompleto._id
-                }
-                res.status(200).json({ status: 200, message: "Sucesso", educador: educador });
-                break;
-            default:
-                console.log("buscaEducador > default > result >>>")
-                console.log(result)
-                // 500 Internal Server Error
-                res.status(500).send({ status: 500, message: "Erro ao buscar Educador" });
-        }
+        // 200 OK
+        res.status(200).json({ status: 200, message: "Sucesso", educador: educador });
     } catch (err){
         console.log("buscaEducador > err >>>")
         console.log(err)
@@ -82,14 +45,8 @@ exports.novoEducador = async (req, res) => {
             usuario:    usuarioExiste._id
         });
 
-        const savedEducador = await newEducador.save();
-        const educador      =   {
-            nome:       usuarioExiste.nome,
-            email:      usuarioExiste.email,
-            papel:      usuarioExiste.papel,
-            _id:        usuarioExiste._id,
-            idEducador: savedEducador._id
-        }
+        const educador = await newEducador.save();
+
         res.status(200).json({ status: 200, message: "Educador cadastrado com sucesso", educador: educador });
     } catch (err){
         console.log("novoEducador > err >>>")
